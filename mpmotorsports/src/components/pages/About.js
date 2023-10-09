d110605af0a63b5f0e8dae274532ea82dbe2337f
AJoelj84 <ajoelj84@gmail.com>
2023-10-09 13:42:00 -0400

    Added to Style.css, and Started constructing the About Page

--- mpmotorsports/src/components/pages/About.js
+++ mpmotorsports/src/components/pages/About.js
@@ -1,23 +1,64 @@
 import React from 'react';
-
+import engine from '../images/engine.jpg';
+import engine2 from '../images/engine2.jpg';
+import engine3 from '../images/engine3.jpg';
+import grinder from '../images/grinder.jpg';
+import hillman from '../images/hillman.jpg';
+import max from '../images/max1.jpg';
+import maxprofile from '../images/maxprofile.jpg';
+import '../assets/style.css';
 
 function About() {
   return (
     <div>
-      <h1 className='bodyheaders' style={{ fontFamily: 'Josefin Sans, sans-serif' }}>About MP Motorsports</h1>
+      <h1 className='aboutheader' style={{ fontFamily: 'Josefin Sans, sans-serif' }}>About MP Motorsports</h1>
       <section style={{ fontFamily: 'Josefin Sans, sans-serif' }} className='bodytext'>
         <div className="row">
           <div className="col-md-6 order-md-2 text-center">
-            Some Place Holder Text heeeeere<br></br>
-            This is probably for a PICTURE
+            <img src={maxprofile} alt= 'maxprofile' className='maxprofile'/>
           </div>
           <div className="col-md-6 order-md-1">
             <div style={{ marginLeft: '80px', marginRight: 'auto' }}>
-              <p>
-               and some place holder text heeeeerreeeee
+              <p className='aboutus'>
+                
+                    At MP Motorsports our number one priority is the relationship we develop with each one of our customers.<br></br>
+                    <br></br>
+                    MP Motorsports aims to offer high-quality auto repair services. 
+                    We recognize the inconvenience and stress involved when your regular 
+                    daily routine is interrupted by the necessity for auto repair, and we are 
+                    committed to making the process personalized, convenient, and easy from start to finish.
+                    <br></br><br></br>
+                    As a company, we strive to give you the highest quality, 
+                    most cost-effective auto repair and to take care of your needs with compassion and kindness. 
+                    We believe in conducting our business the same way we live our lives - with honesty, integrity, 
+                    and fairness.
+                    <br></br><br></br>
+                    Not only do we affirm the unique worth of each one of our customers, 
+                    but we also recognize each MP Motorsports  employee as a valuable member of our family and strive 
+                    to treat each other with the same loyalty, respect, and dignity we give to you.
+                    <br></br><br></br>
+                    <br></br><br></br>
+ 
+
+                    Additionally, MP    Motorsports is technologically savvy with computerized diagnostic services, 
+                    to ensure that the autos we service get the latest, most up to date technology used, 
+                    while keeping customer costs to a minimum. Finally, 
+                    MP Motorsports  has strong vendor relationships with the most service conscious vendors 
+                    who are capable of delivering our products in a short amount of time, which cuts the service 
+                    time and we then can pass this on to our customers.
               </p>
-              
             </div>
+          </div>
+        </div>
+
+        <div className='gallery-container'>
+          <div className='gallery'>
+            <img src={engine} alt='shopimages' className='galleryimage' />
+            <img src={engine2} alt='shopimages' className='galleryimage' />
+            <img src={engine3} alt='shopimages' className='galleryimage' />
+            <img src={grinder} alt='shopimages' className='galleryimage' />
+            <img src={hillman} alt='shopimages' className='galleryimage' />
+            <img src={max} alt='shopimages' className='galleryimage' />
           </div>
         </div>
       </section>
